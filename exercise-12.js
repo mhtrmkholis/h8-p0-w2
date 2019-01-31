c63ef1229a7534e84a29f1cdcbc11e033f09f307
mhtrmkholis <46910884+mhtrmkholis@users.noreply.github.com>
2019-01-31 20:28:54 +0700

    Update exercise-12.js

--- exercise-12.js
+++ exercise-12.js
@@ -1,29 +1,12 @@
 function konversiMenit(menit) {
-  var jam = "1:"
-  var menitBaru = menit - 60
-
-  if (menit >= 60 && menit <= 69 && menit < 120) {
-    return jam + "0" + menitBaru;
+    var sisaMenit = menit % 60;
+    if(sisaMenit < 10) {
+      sisaMenit = "0" + menit % 60;
+    }
+    return Math.floor(menit / 60) + ":" + sisaMenit;
   }
-  else if (menit >= 60 && menit >= 70 && menit < 120) {
-    return jam + menitBaru;
-  }
-  else if (menit >= 120 && menit <= 129) {
-    jam = "2:"
-    menitBaru = menit - 120;
-    return jam + "0" + menitBaru;
-  }
-  else if (menit >= 130) {
-    jam = "2:"
-    menitBaru = menit - 120;
-    return jam + menitBaru;
-  }
-  else {
-    return "0:" + menit;
-  }
-}
-console.log(konversiMenit(63));
-console.log(konversiMenit(124));
-console.log(konversiMenit(53));
-console.log(konversiMenit(88));
-console.log(konversiMenit(120));
+  console.log(konversiMenit(63));
+  console.log(konversiMenit(124));
+  console.log(konversiMenit(53));
+  console.log(konversiMenit(88));
+  console.log(konversiMenit(120));
