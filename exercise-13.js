43c444fb00223573f0c0b5a7760c1114875f667f
mhtrmkholis <46910884+mhtrmkholis@users.noreply.github.com>
2019-02-03 07:28:40 +0700

    add exercise-13.js

---
+++ exercise-13.js
@@ -0,0 +1,21 @@
+function xo(str) {
+    var countX = 0;
+    var countO = 0;
+
+    for(var i = 0;i < str.length;i++){
+        if (str[i] == "x"){
+            countX += 1;
+        } else if (str[i] == "o") {
+            countO += 1;
+        }
+    }
+
+    return countX == countO;
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
